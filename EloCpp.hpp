40045dacfc9de65c102bb60c2d811f6d8908c622
Charles Mailly <charles.mailly@free.fr>
2014-10-23 18:41:38 +0200

    Up To Date

--- EloCpp.hpp
+++ EloCpp.hpp
@@ -7,8 +7,8 @@ namespace elo
 class Player
 {
     public:
-        Player() { mRank = basicRank; mGamePlayed = 0; mOverpassed = false; }
-		Player(float rank, float gamePlayed, bool overpassed) : mRank(rank), mGamePlayed(gamePlayed),mOverpassed(overpassed) {}
+        Player() { mRank = Manager::getBasicRank(); mGamePlayed = 0; mOverpassed = false; }
+	Player(float rank, int gamePlayed, bool overpassed) : mRank(rank), mGamePlayed(gamePlayed),mOverpassed(overpassed) {}
 
         float versus(float const& oRank, Result const& result)
         {
@@ -20,11 +20,14 @@ class Player
             return mRank;
         }
 
-        float getRank() const { return mRank; }
+        int getRank() const { return static_cast<int>(mRank); }
+        float getRankF() const { return mRank; }
+        int getGamePlayed() const { return mGamePlayed; }
+        bool hasOverpassed() const { return mOverpassed; }
 
     private:
-        float   getE(float const& oRank) { return getQ(mRank) / (getQ(mRank) + getQ(oRank)); }
-        float   getS(Result const& result)
+        float getE(float const& oRank) { return getQ(mRank) / (getQ(mRank) + getQ(oRank)); }
+        float getS(Result const& result)
         {
             switch (result)
             {
@@ -33,7 +36,7 @@ class Player
                 default: return 0;
             }
         }
-        float   getK()
+        float getK()
         {
             if (mGamePlayed < 30)
                 return 30;
@@ -41,7 +44,7 @@ class Player
                 return 15;
             return 10;
         }
-        float   getQ(float rank) { return pow(10,rank/400); }
+        float getQ(float rank) { return pow(10,rank/400); }
 
         void update()
         {
@@ -56,51 +59,53 @@ class Player
 
     private:
         float mRank;
-        float mGamePlayed;
+        int mGamePlayed;
         bool  mOverpassed;
 
-}; // CLASS PLAYER
+};
 
 class Manager
 {
-	public:
-		static void game(Player& a, Player&b, Result const& result = Win)
-		{
-			if (result == Win)
-			{
-				float rankA = a.getRank();
-				a.versus(b.getRank(), Win);
-				b.versus(rankA, Lose);
-			}
-			else if (result == Draw)
-			{
-				float rankA = a.getRank();
-				a.versus(b.getRank(), Draw);
-				b.versus(rankA, Draw);
-			}
-			else
-			{
-				game(b,a,Win);
-			}
-		}
-		static void setScoreToOverpass(int score) { ScoreToOverpass = score; }
-		static void setBasicRank(int rank) { BasicRank = rank; }
-		static int getScoreToOverpass() { return ScoreToOverpass; }
-		static int getBasicRank() { return BasiRank; }
+    public:
+	static void game(Player& a, Player&b, Result const& result = Win)
+	{
+	    if (result == Win)
+	    {
+		float rankA = a.getRankF();
+		a.versus(b.getRankF(), Win);
+		b.versus(rankA, Lose);
+	    }
+	    else if (result == Draw)
+	    {
+		float rankA = a.getRankF();
+		a.versus(b.getRankF(), Draw);
+		b.versus(rankA, Draw);
+	    }
+	    else
+	    {
+		game(b,a,Win);
+	    }
+	}
+	static void setScoreToOverpass(int score) { ScoreToOverpass = static_cast<float>(score); }
+	static void setScoreToOverpass(float score) { ScoreToOverpass = score; }
+	static void setBasicRank(int rank) { BasicRank = static_cast<float>(rank); }
+	static void setBasicRank(float rank) { BasicRank = rank; }
+	static float getScoreToOverpass() { return ScoreToOverpass; }
+	static float getBasicRank() { return BasiRank; }
 		
-		enum Result
-		{
-			Win,
-			Draw,
-			Lose,
-		}	
+	enum Result
+	{
+	    Win,
+            Draw,
+	    Lose,
+	};	
 		
-	private:
-		static int ScoreToOverPass;
-		static int BasicRank;
+    private:
+	static float ScoreToOverPass;
+	static float BasicRank;
 
-}; // CLASS MANAGER
+};
 
-} // NAMESPACE ELO
+}
 
 #endif // ELOCPP_HPP_INCLUDED
